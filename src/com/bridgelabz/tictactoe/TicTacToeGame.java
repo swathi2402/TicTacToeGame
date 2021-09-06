81b4471d9ecf9065fe8788d94df4aaf836bd48ae
Swathi <swathi.h@perfios.com>
2021-09-06 08:40:04 +0530

    MODIFIED: Including return statement for method

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -5,14 +5,15 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		System.out.println("*** Welcome to Tic Tac Toe Game ***");
 		
-		startGame();
+		char[] board = startGame();
 	}
 
-	private static void startGame() {
+	private static char[] startGame() {
 		char[] ticTacBoard = new char[10];
 		for(int index = 0; index < ticTacBoard.length; index++) {
 			ticTacBoard[index] = ' ';
 		}
+		return ticTacBoard;
 	}
 
 }
