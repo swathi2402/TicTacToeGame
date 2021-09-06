81856f5e0c2dd042d445d433cb4d2a8fac193d0d
Swathi <swathi.h@perfios.com>
2021-09-06 17:48:22 +0530

    ADDED: Method to navigate to desired position to add symbol

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,8 +1,14 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToeGame {
+
+	static Scanner scanner = new Scanner(System.in);
+	static char[] board;
+	static char playerSymbol;
+	static char computerSymbol;
 
 	private static char[] startGame() {
 		char[] ticTacBoard = new char[10];
@@ -13,15 +19,21 @@ public class TicTacToeGame {
 	}
 
 	private static char getInput() {
-		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter charecter 'X' or 'O'");
-		char playerSymbol = scanner.next().toUpperCase().charAt(0);
-		scanner.close();
-		return playerSymbol;
+		return scanner.next().toUpperCase().charAt(0);
 	}
 
+	private static void move() {
+		System.out.println("Enter the position 1 to 9 to move");
+		int playerPosition = scanner.nextInt();
+		int computerPosition = new Random().nextInt(9 - 1 + 1) + 1;
+		board[playerPosition] = playerSymbol;	
+		board[computerPosition] = computerSymbol;
+		showBoard(board);
+	}
+		
 	private static void showBoard(char[] board) {
-		System.out.println("Board");
+		System.out.println("Board: ");
 		for (int index = 1; index < 10; index++) {
 			System.out.print(board[index] + " | ");
 			if (index % 3 == 0) {
@@ -33,10 +45,9 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		System.out.println("*** Welcome to Tic Tac Toe Game ***");
 
-		char[] board = startGame();
-		char playerSymbol = getInput();
-		char computerSymbol = (playerSymbol == 'X') ? 'O' : 'X';
-		showBoard(board);
+		board = startGame();
+		playerSymbol = getInput();
+		computerSymbol = (playerSymbol == 'X') ? 'O' : 'X';
+		move();
 	}
-
 }
