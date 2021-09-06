b3e45c62d34a627fa2bbefdebbcd1d500fe89d7a
Swathi <swathi.h@perfios.com>
2021-09-06 08:46:27 +0530

    ADDED: Method to take input from user

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,19 +1,31 @@
 package com.bridgelabz.tictactoe;
 
-public class TicTacToeGame {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		System.out.println("*** Welcome to Tic Tac Toe Game ***");
-		
-		char[] board = startGame();
-	}
+public class TicTacToeGame {
 
 	private static char[] startGame() {
 		char[] ticTacBoard = new char[10];
-		for(int index = 0; index < ticTacBoard.length; index++) {
+		for (int index = 0; index < ticTacBoard.length; index++) {
 			ticTacBoard[index] = ' ';
 		}
 		return ticTacBoard;
 	}
+
+	private static String getInput() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter charecter 'X' or 'O'");
+		String sign = scanner.nextLine();
+		scanner.close();
+		return sign;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("*** Welcome to Tic Tac Toe Game ***");
+
+		char[] board = startGame();
+		String inputSign = getInput();
+	}
+
 
 }
