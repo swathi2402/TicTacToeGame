e8ed817c5f23df39fa8de956c7521ab912864e50
Swathi <swathi.h@perfios.com>
2021-09-06 08:24:47 +0530

    ADDED: Method to start game using char array

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -4,6 +4,15 @@ public class TicTacToeGame {
 
 	public static void main(String[] args) {
 		System.out.println("*** Welcome to Tic Tac Toe Game ***");
+		
+		startGame();
+	}
+
+	private static void startGame() {
+		char[] ticTacBoard = new char[10];
+		for(int index = 0; index < ticTacBoard.length; index++) {
+			ticTacBoard[index] = ' ';
+		}
 	}
 
 }
